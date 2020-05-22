acc4ee4ac3142eeaaa503d08c483c4f7c2560ce2
suyan <875664456@qq.com>
2020-05-22 21:20:46 +0800

    Ss

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <homeheader></homeheader>
+  </div>
+</template>
+
+<script>
+import homeHeader from './home/components/Header'
+export default {
+  name: 'Home',
+  components: homeHeader
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+      <div class="header-left">返回</div>
+      <div class="header-input">输入城市经典游玩主题</div>
+      <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background: #00bcd4
+   .header-left
+     float:left
+     width:.64rem
+   .header-input
+     flex:1
+     margin-top: .12rem
+     height:.64rem
+     margin-left:.2rem
+     background:#fff
+     border-radius:.1rem
+     color : #ccc
+     line-height:.64rem
+   .header-right
+     float:right
+     width: 1.24rem
+     text-align: center
+</style>
